304c2439e6128d32effaaa712c8b45a5bd5430fd
Jan Kaczmarek <janjakubkaczmarek@gmail.com>
2020-02-09 15:13:23 +0100

    added basic router

---
+++ src/Components/gamePage/gamePage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import TopNavBar from "../topNavBar/topNavBar";
+
+class GamePage extends Component {
+    render() {
+        return (
+            <div className="gamePage">
+                <TopNavBar/>
+                <div className="content">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default GamePage;
