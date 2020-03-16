58cfd0f656fe3a5e638a733eb5c68a439f80ed33
Jan Kaczmarek <janjakubkaczmarek@gmail.com>
2020-03-16 11:48:51 +0100

    repaired player

--- src/Components/Game/Card/Card.js
+++ src/Components/Game/Card/Card.js
@@ -21,9 +21,19 @@ class Card extends Component {
             })
     }
 
+    componentDidUpdate(prevProps) {
+        if (this.props.artist.id !== prevProps.artist.id) {
+            getArtistTracks(this.props.artist.id)
+                .then(res =>{
+                    this.setState({tracks: res.tracks, loading: false});
+                })
+        }
+    }
+
     render() {
         let card = <Loader/>;
         if(!this.state.loading) {
+            console.log("Card rerender");
             card = (<div>
                         <img className="photo" alt="artist" src={this.props.artist.images[0].url}/>
                         <h2 className="artist-name">{this.props.artist.name}</h2>
@@ -34,7 +44,7 @@ class Card extends Component {
                             <Player trackPrev={this.state.tracks[0]}/>
                             <Player trackPrev={this.state.tracks[1]}/>
                             <Player trackPrev={this.state.tracks[2]}/>
-                    </div>)
+            </div>)
         }
         return(
             <div className="cardTemplate">
