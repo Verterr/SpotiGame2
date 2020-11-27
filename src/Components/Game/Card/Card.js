59e68190f6ce1413b2f0af663b9004bcd1d18628
Jan Kaczmarek <janjakubkaczmarek@gmail.com>
2020-11-27 22:57:08 +0100

    fixed card template

--- src/Components/Game/Card/Card.js
+++ src/Components/Game/Card/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 
 import Player from "../Player/Player";
 import Loader from "../Loader/Loader";
-import {Card as CardTemplate, CardHeader, CardContent} from "@material-ui/core";
+import {Card as CardTemplate, CardHeader, CardContent, List} from "@material-ui/core";
 import CardMedia from "@material-ui/core/CardMedia";
 
 import {getArtistTracks} from "../../../Containers/Game/gameLogic";
@@ -35,17 +35,19 @@ class Card extends Component {
     render() {
         let card = <Loader/>;
         if(!this.state.loading) {
-            console.log(this.props.artist.images);
             card = (<div>
                     <CardTemplate className="cardTemplate">
                         <CardHeader className="artistHeader" title={this.props.artist.name} subheader={`Genres: ${this.props.artist.genres.filter((value, i) => i<=3).join(', ')}`}/>
                         <div className="artistPhoto">
                             <img src={this.props.artist.images[1].url} alt={this.props.artist.name}/>
                         </div>
-                        <CardContent className="cardPlayers">
-                            <Player trackPrev={this.state.tracks[0]} volume={this.props.volume}/>
-                            <Player trackPrev={this.state.tracks[1]} volume={this.props.volume}/>
-                            <Player trackPrev={this.state.tracks[2]} volume={this.props.volume}/>
+                        <CardContent>
+                            <List className="cardPlayers">
+                                <Player trackPrev={this.state.tracks[0]} volume={this.props.volume}/>
+                                <Player trackPrev={this.state.tracks[1]} volume={this.props.volume}/>
+                                <Player trackPrev={this.state.tracks[2]} volume={this.props.volume}/>
+                                <Player trackPrev={this.state.tracks[3]} volume={this.props.volume}/>
+                            </List>
                         </CardContent>
                     </CardTemplate>
                     </div>)
--- src/Components/Game/Player/Player.js
+++ src/Components/Game/Player/Player.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
-
 import './Player.css';
-
 import ReactHowler from 'react-howler';
-import {Fab} from "@material-ui/core";
+import {ListItem, ListItemAvatar, ListItemText} from "@material-ui/core";
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
+
 class Player extends Component {
 
     state = {
@@ -19,25 +18,26 @@ class Player extends Component {
     };
 
     render() {
-        let icon = <PlayArrowIcon/>
+        console.log(this.props.trackPrev);
+        let icon = <PlayArrowIcon className="playerIcon" onClick={() => this.playHandler(!this.state.playing)}/>
         if(this.state.playing) {
-            icon = <PauseIcon/>
+            icon = <PauseIcon className="playerIcon" onClick={() => this.playHandler(!this.state.playing)}/>
         }
         return (
-            <div className="player">
-                {/*<h3>{this.props.trackPrev.name}</h3>*/}
-                <h3>{this.props.trackPrev.name}</h3>
-                <Fab className="playButton" onClick={() => this.playHandler(!this.state.playing)}>
-                    {icon}
-                    <ReactHowler
-                        format={['mp3']}
-                        src={this.props.trackPrev.preview_url}
-                        playing={this.state.playing}
-                        ref={(ref) => (this.player = ref)}
-                        volume={this.props.volume}
-                    />
-                </Fab>
-            </div>
+            <ListItem className="player">
+                <ListItemAvatar>
+                        <img className="albumImg" src={this.props.trackPrev.album.images[1].url}/>
+                </ListItemAvatar>
+                <ListItemText className="playerName" primary={this.props.trackPrev.name} secondary={this.props.trackPrev.album.name} />
+                {icon}
+                <ReactHowler
+                    format={['mp3']}
+                    src={this.props.trackPrev.preview_url}
+                    playing={this.state.playing}
+                    ref={(ref) => (this.player = ref)}
+                    volume={this.props.volume}
+                />
+            </ListItem>
         )
     }
 };
